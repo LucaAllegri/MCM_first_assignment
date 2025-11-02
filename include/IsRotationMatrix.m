ac56304548db96b51751d6989b6f88f247cbcae4
Luca Allegri <luca.allegri02@gmail.com>
2025-11-02 18:11:38 +0100

    added use of det function

--- include/IsRotationMatrix.m
+++ include/IsRotationMatrix.m
@@ -17,8 +17,6 @@ function [isRotationMatrix] = IsRotationMatrix(R)
     end
     
     % Calcolo del determinante, deve essere uguale a 1
-
-    %det_R = R(1,1) * (R(2,2)*R(3,3) - R(2,3)*R(3,2)) -R(1,2) * (R(2,1)*R(3,3) - R(2,3)*R(3,2)) + R(1,3) * (R(2,1)*R(3,2) - R(2,2)*R(3,1));
     det_R = det(R);
     disp("determinante= ");
     disp(det_R);
@@ -32,26 +30,4 @@ function [isRotationMatrix] = IsRotationMatrix(R)
     end
 
     %%check identity matrix and determinant
-end
-
-function deter = det(R)
-    
-    [n,m] = size(R);
-
-    if n == 1
-        deter = R(n);
-        return;
-    elseif n == 2
-        deter = (R(1,1)*R(2,2)) - (R(1,2)*R(2,1));
-        return;
-    else
-        deter=0;
-        for j=1:n
-            M = R(2:n,[1:j-1,j+1:m]);
-            segno = (-1)^(1+j);
-            deter =  deter + segno * R(1,j) * det(M);
-        end
-
-    end
- 
 end
