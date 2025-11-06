ee8477a9717923a8d57aae190a01a693063181b5
Luca Allegri <luca.allegri02@gmail.com>
2025-11-06 09:25:16 +0100

    Gimbal Lock control in RotToYPR function

--- include/RotToYPR.m
+++ include/RotToYPR.m
@@ -3,13 +3,24 @@ function [psi,theta,phi] = RotToYPR(R)
     isRotation = IsRotationMatrix(R);
 
     if isRotation
-        theta = atan2 (-R(3,1), sqrt((R(1,1))^2 + (R(2,1))^2)); 
-        psi = atan2 (R(2,1), R(1,1)); 
-        phi = atan2 (R(3,2), R(3,3));
+        theta = atan2 (-R(3,1), sqrt((R(1,1))^2 + (R(2,1))^2));
+        if theta > (-pi/2) + 10^-3 && theta < (pi/2) - 10^-3
+            psi = atan2 (R(2,1), R(1,1)); 
+            phi = atan2 (R(3,2), R(3,3));
+        elseif -pi/2 <= theta && theta <= -pi/2 + 10^-3
+            disp("Gimbal Lock, theta=-pi/2")
+            psi = 0;
+            phi = atan2 (R(1,2), R(1,3));
+        elseif (pi/2) - 10^-3 <= theta && theta <= pi/2
+            disp("Gimbal Lock, theta=pi/2")
+            psi = 0;
+            phi = atan2 (-R(1,2), R(1,3));
+        end
     else
-        theta = -1;
-        phi = -1;
-        psi = -1;
+        disp("The matrix is not rotable");
+        theta = null;
+        phi = null;
+        psi = null;
     end
 
 end
