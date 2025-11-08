14bd565477787f43a177280e2a10047316505d36
Luca Allegri <luca.allegri02@gmail.com>
2025-11-08 18:44:41 +0100

    insered ex1.6

--- main.m
+++ main.m
@@ -348,3 +348,98 @@ end
 
 
 %%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% PUNTO 1.6    Frame tree
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Transformation matrx frame <1> wrt <world>
+T01 = [ 1  0  0   0;
+        0  1  0   0;
+        0  0  1 175;
+        0  0  0   1 ];
+[R1,p1] = extractRT(T01);
+
+% Transformation matrx frame <2> wrt <1>
+T12 = [-1  0  0   0;
+        0  0  1   0;
+        0  1  0  98;
+        0  0  0   1 ];
+T = T01*T12;
+[R2,p2] = extractRT(T);
+
+% Transformation matrx frame <3> wrt <2>
+T23 = [ 0  0  1 105;
+        0  1  0   0;
+       -1  0  0   0;
+        0  0  0   1 ];
+T = T01*T12*T23;
+[R3,p3] = extractRT(T);
+
+% Transformation matrx frame <4> wrt <3>
+T34 = [ 0     0   -1     0;
+        0    -1    0   145.50;
+       -1     0    0   326.50;
+        0     0    0     1];
+
+% Transformation matrx frame <5> wrt <4>
+T45 = [ 0     0    1     35;
+       -1     0    0      0;
+        0    -1    0      0;
+        0     0    0      1];
+
+% Transformation matrx frame <6> wrt <5>
+T56 = [ 0     1    0      0;
+        0     0    1      0;
+        1     0    0    385;
+        0     0    0      1];
+
+% Transformation matrx frame <7> wrt <6>
+T67 = [ 0     0    1    153;
+        1     0    0      0;
+        0     1    0      0;
+        0     0    0      1];
+
+a = 30;
+
+T1 = T01;          
+T2 = T1 * T12;     
+T3 = T2 * T23;    
+T4 = T3 * T34;    
+T5 = T4 * T45;     
+T6 = T5 * T56;     
+T7 = T6 * T67;   
+
+Tlist = {T1,T2,T3,T4,T5,T6,T7};
+labels = { 'X_1','X_2','X_3','X_4','X_5','X_6','X_7';
+           'Y_1','Y_2','Y_3','Y_4','Y_5','Y_6','Y_7';
+           'Z_1','Z_2','Z_3','Z_4','Z_5','Z_6','Z_7'};
+
+% World Frame
+plotframe('LabelBasis', true, ...
+          'Labels', {'X_{world}','Y_{world}','Z_{world}'}, ...
+          'TextProperties', {'FontAngle','italic'})
+
+for k = 1:7
+    [R, p] = extractRT(Tlist{k});
+    plotframe(R, p/a, 'MatrixIndexing','columnmajor', 'LabelBasis', true, ...
+              'Labels', labels(:,k)', ...
+              'TextProperties', {'FontAngle','italic'})
+end
+view(0, 0);
+
+figure
+axes('DataAspectRatio', [1 1 1], 'View', [37.5 30])
+hold on, grid on
+
+%  World Frame
+plotframe('LabelBasis', true, ...
+          'Labels', {'X_{world}','Y_{world}','Z_{world}'}, ...
+          'TextProperties', {'FontAngle','italic'})
+
+for k = 1:7
+    [R, p] = extractRT(Tlist{k});
+    plotframe(R, p/a, 'MatrixIndexing','columnmajor', 'LabelBasis', true, ...
+              'Labels', labels(:,k)', ...
+              'TextProperties', {'FontAngle','italic'})
+end
