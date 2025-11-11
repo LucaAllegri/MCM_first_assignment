0cd307e967268ca11dd8e54cabb573ef3a9485c3
AntonioZerbato <antozerba28@gmail.com>
2025-11-11 16:36:59 +0100

    final commit

--- include/eigen_H.m
+++ include/eigen_H.m
@@ -10,19 +10,19 @@ function [h] = eigen_H(R)
     h = eigVec(:,ind);
     h = h/norm(h);
 
-    %corretto allineamento di h
+    %corretto segno di h
     %eig(R) può ritornare sia l' autovettore positivo che negativo: 
     % Rh = h ma anche R(-h) = -h con entrambi autovalore 1.
     %Per convenzione eig ritorna sempre h quindi, per tenere la conveznione
     %di rotazione positiva usata in RotToAngleAxis dobbiamo tenere conto
     %delle direzione nel vettore h
     
-    % %calcolo l'h con RotToAngleAxis supponendo una rotazione positiva
-    % [h_rod, ~] = RotToAngleAxis(R);
-    % 
-    % %confronto per capire l'orientamento corretto di h
-    % if dot(h, h_rod) < 0
-    %     h = -h;
-    % end
+    %calcolo l'h con RotToAngleAxis supponendo una rotazione positiva
+    [h_rod, ~] = RotToAngleAxis(R);
+
+    %confronto per capire l'orientamento corretto di h
+    if dot(h, h_rod) < 0
+        h = -h;
+    end
     
 end
